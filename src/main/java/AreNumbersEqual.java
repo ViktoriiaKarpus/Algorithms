c0babfd6e575db00649360a2ec0af84752ad5ef2
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2022-07-25 23:08:41 +0200

    Update AreNumbersEqual.java

--- src/main/java/AreNumbersEqual.java
+++ src/main/java/AreNumbersEqual.java
@@ -1,18 +1,5 @@
 public class AreNumbersEqual {
-
-    //Напишите алгоритм AreNumbersEqual, который принимает на вход 2 любых int числа, и возвращает
-    //0, если числа равны
-    //-1, если первое число меньше второго
-    //1, если первое число больше второго
-    //
-    //Test Data:
-    //89, 89
-    //Expected result: 0
-    //-89, 89
-    //Expected result: -1
-    //89, -89
-    //Expected result: 1
-
+    
     public static int AreNumbersEqual(int a, int b) {
 
             if (a == b){
